0e1f825b0f4893164878306817b8b17948ad3a3f
Ganesh Raut <ganeshraut9689@gmail.com>
2026-04-17 02:19:12 +0530

    Transaction history screen added with some handy filters at top.

--- src/axios/axiosInstance.tsx
+++ src/axios/axiosInstance.tsx
@@ -2,8 +2,10 @@
 import axios from 'axios';
 import MockAdapter from 'axios-mock-adapter';
 import accountsData from '../data/accounts.json';
+import transactionsData from '../data/transactions.json';
 import { Account } from '../types/account';
 import cardsData from '../data/cards.json';
+import { Transaction } from '../types/Transaction';
 
 const axiosInstance = axios.create({
     baseURL: 'http://localhost', // baseURL is required but not actually used
@@ -30,6 +32,14 @@ mock.onGet(/\/cards\/\w+/).reply(config => {
     const id = config.url?.split('/').pop();
     const card = cardsData.find(c => c.id === id);
     return card ? [200, card] : [404];
+});
+
+// ✅ Transactions
+mock.onGet('/transactions').reply(200, transactionsData);
+mock.onGet(/\/transactions\/\w+/).reply(config => {
+    const id = config.url?.split('/').pop();
+    const transaction = transactionsData.find(t => t.id === id);
+    return transaction ? [200, transaction] : [404];
 });
 
 export default axiosInstance;
